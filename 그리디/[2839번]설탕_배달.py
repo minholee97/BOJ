019d2a417b0d8e5f275f79bba88ce0a25dcb30d0
minholee <76832861+MinhoLee2@users.noreply.github.com>
2021-01-02 20:56:53 +0900

    그리디 알고리즘 문제

---
+++ 그리디/[2839번]설탕_배달.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/2839
+
+N = int(input())
+result = 0
+while True:
+	if N % 5 == 0:
+		result += N // 5
+		break
+	else:
+		result += 1
+		N -= 3
+		if N < 0:
+			result = -1
+			break
+print(result)
