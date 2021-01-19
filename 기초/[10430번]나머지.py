56c0733f3488814c8aeccf841998bc5343288e79
minholee <76832861+MinhoLee2@users.noreply.github.com>
2021-01-19 20:14:13 +0900

    기초

---
+++ 기초/[10430번]나머지.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/10430
+
+import sys
+
+A, B, C = map(int, sys.stdin.readline().split())
+
+print((A+B)%C)
+print(((A%C)+(B%C))%C)
+print((A*B)%C)
+print(((A%C)*(B%C))%C)
